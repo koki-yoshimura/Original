575b6d53a84095272a87229fedc889a4429cfc22
koki-yoshimura <fanta1536arm@gmail.com>
2019-05-05 17:04:04 +0900

    no message

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,7 +8,9 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody myRigidbody;
 
     //上下に移動するための力
-    private float moveForce = 500.0f;
+    private float moveForce = 14.0f;
+
+    public GameObject atackPrefab;
 
     //左ボタン押下の判定
     private bool isUButtonDown = false;
@@ -16,11 +18,18 @@ public class PlayerController : MonoBehaviour {
     //右ボタン押下の判定
     private bool isDButtonDown = false;
 
+    private bool isAButtonDown = false;
+
+
+
 
     // Use this for initialization
     void Start () {
-		
-	}
+
+        //Rigidbodyコンポーネントを取得（追加）
+        this.myRigidbody = GetComponent<Rigidbody>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -37,6 +46,11 @@ public class PlayerController : MonoBehaviour {
             this.myRigidbody.AddForce(0, -this.moveForce, 0);
         }
 
+        if (this.isAButtonDown)
+        {
+            GameObject atack = Instantiate(atackPrefab) as GameObject;
+            atack.transform.position = new Vector3(this.transform.position.x + 1, this.transform.position.y,0 );
+        }
     }
 
     //上ボタンを押し続けた場合の処理
@@ -59,5 +73,16 @@ public class PlayerController : MonoBehaviour {
     public void GetMyDownButtonUp()
     {
         this.isDButtonDown = false;
+    }
+
+    //上ボタンを押し続けた場合の処理
+    public void GetMyAButtonDown()
+    {
+        this.isAButtonDown = true;
+    }
+    //上ボタンを離した場合の処理
+    public void GetMyAButtonUp()
+    {
+        this.isAButtonDown = false;
     }
 }
