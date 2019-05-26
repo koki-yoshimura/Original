664a0afe089f516e9478f4c94e365426e3cc7e0f
koki-yoshimura <fanta1536arm@gmail.com>
2019-05-26 22:19:51 +0900

    no message

--- Assets/script/ShopController.cs
+++ Assets/script/ShopController.cs
@@ -4,13 +4,94 @@ using UnityEngine;
 
 public class ShopController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    Vector3 StartPos;
+
+    private bool OneButtonDown = false;
+
+    private bool TwoButtonDown = false;
+
+    private bool ThreeButtonDown = false;
+
+    private bool FourButtonDown = false;
+
+    private bool FiveButtonDown = false;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.StartPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 EndPos = Input.mousePosition;
+        }
 		
+
 	}
+
+    //1ボタンを押し続けた場合の処理
+    public void GetMyOneButtonDown()
+    {
+        this.OneButtonDown = true;
+    }
+
+    //1ボタンを離した場合の処理
+    public void GetMyOneButtonUp()
+    {
+        this.OneButtonDown = false;
+    }
+
+    //2ボタンを押し続けた場合の処理
+    public void GetMyTwoButtonDown()
+    {
+        this.TwoButtonDown = true;
+    }
+
+    //2ボタンを離した場合の処理
+    public void GetMyTwoButtonUp()
+    {
+        this.TwoButtonDown = false;
+    }
+
+    //3ボタンを押し続けた場合の処理
+    public void GetMyThreeButtonDown()
+    {
+        this.ThreeButtonDown = true;
+    }
+
+    //3ボタンを離した場合の処理
+    public void GetMyThreeButtonUp()
+    {
+        this.ThreeButtonDown = false;
+    }
+
+    //4ボタンを押し続けた場合の処理
+    public void GetMyFourButtonDown()
+    {
+        this.FourButtonDown = true;
+    }
+
+    //4ボタンを離した場合の処理
+    public void GetMyFourButtonUp()
+    {
+        this.FourButtonDown = false;
+    }
+
+    //5ボタンを押し続けた場合の処理
+    public void GetMyFiveButtonDown()
+    {
+        this.FiveButtonDown = true;
+    }
+
+    //5ボタンを離した場合の処理
+    public void GetMyFiveButtonUp()
+    {
+        this.FiveButtonDown = false;
+    }
 }
