42e9f84e9e4fac12b8f7e56c4c217a51552d0d99
koki-yoshimura <fanta1536arm@gmail.com>
2019-06-16 00:11:26 +0900

    no message

--- Assets/script/SoubiController.cs
+++ Assets/script/SoubiController.cs
@@ -22,8 +22,32 @@ public class SoubiController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (toggle.isOn)
+        {
+            if (toggle.gameObject.tag == "toggleA")
+            {
+                SoubiA += 1;
+                SoubiB = 0;
+            }
+            else if (toggle.gameObject.tag == "toggleB")
+            {
+                SoubiB += 1;
+                SoubiA = 0;
+            }
+            else if (toggle.gameObject.tag == "toggleD")
+            {
+                SoubiC += 1;
+                SoubiD = 0;
+            }
+            else if (toggle.gameObject.tag == "toggleE")
+            {
+                SoubiD += 1;
+                SoubiC = 0;
+            }
+
+        }
+
+    }
 
     public void OnValueChanged(UnityEngine.UI.Toggle toggle)
     {
@@ -40,35 +64,21 @@ public class SoubiController : MonoBehaviour {
 
     public void SoubiAbuttondown(UnityEngine.UI.Toggle toggle)
     {
-        if (toggle.isOn)
-        {
-            SoubiA += 1;
-        }
+
     }
 
     public void SoubiBbuttondown(UnityEngine.UI.Toggle toggle)
     {
-        if (toggle.isOn)
-        {
-            SoubiB += 1;
-        }
+
     }
 
     public void SoubiDbuttondown(UnityEngine.UI.Toggle toggle)
     {
-        if (toggle.isOn)
-        {
-            SoubiD += 1;
-            SoubiE -= 1;
-        }
+
     }
 
     public void SoubiEbuttondown(UnityEngine.UI.Toggle toggle)
     {
-        if (toggle.isOn)
-        {
-            SoubiE += 1;
-            SoubiD -= 1;
-        }
+
     }
 }
