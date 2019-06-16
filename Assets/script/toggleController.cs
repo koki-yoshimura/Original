ac35dd0213b486aaf5af45538e8526ac56175c3d
koki-yoshimura <fanta1536arm@gmail.com>
2019-06-16 16:24:58 +0900

    no message

---
+++ Assets/script/toggleController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+[RequireComponent(typeof(ToggleGroup))]
+public class toggleController : MonoBehaviour
+{
+    public void OnClick()
+    {
+        // どの Toggle にチェックが入っているか調べる
+        GameObject go = GetComponent<ToggleGroup>().ActiveToggles().FirstOrDefault().gameObject;    // go はチェックが入っている Toggle の GameObject
+        Debug.Log(go.name + " がチェックされている");
+
+        // 例えばチェックされている Toggle の名前で処理を分岐させる
+        switch (go.name)
+        {
+            case "Toggle (0)":
+                Debug.Log("ここにToggle (0)用の処理を書く");
+                break;
+            case "Toggle (1)":
+                Debug.Log("ここにToggle (1)用の処理を書く");
+                break;
+            case "Toggle (2)":
+                Debug.Log("ここにToggle (2)用の処理を書く");
+                break;
+            default:
+                break;
+        }
+    }
+}
