f7964316c87c10c17e6fd834ec67f45041a1daf2
koki-yoshimura <fanta1536arm@gmail.com>
2020-01-28 22:48:55 +0900

    新規: ダイアログの処理を追加

--- Assets/script/CommonDialog.cs
+++ Assets/script/CommonDialog.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class script : MonoBehaviour
+public class CommonDialog : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,5 +15,10 @@ public class script : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void XButtonDown()
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/script/TitleController.cs
+++ Assets/script/TitleController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class TitleController : MonoBehaviour {
 
@@ -11,12 +10,10 @@ public class TitleController : MonoBehaviour {
     [SerializeField] private GameObject GashaButton;
     [SerializeField] private GameObject ShootingButton;
     [SerializeField] private GameObject Title;
-
     [SerializeField] private AudioSource audioSource;
-
     [SerializeField] private GameObject goDialogRoot = null;
+    [SerializeField] private CommonDialog commonDialog = null;
 
-    GameObject  XButton;
 
     private GameObject dialogObj = null;
 
@@ -31,11 +28,8 @@ public class TitleController : MonoBehaviour {
         GashaButton.SetActive(true);
         ShootingButton.SetActive(true);
 
-            var dialog = (GameObject)Resources.Load("Prefabs/CommonDialog");
-            dialogObj = Instantiate(dialog, goDialogRoot.transform, worldPositionStays: false);
-            //this.XButton = GameObject.Find("XButton");
-            //this.XButton.AddComponent<Button>();
-            //this.XButton.onClick.AddListener(XButtonDown);
+            //var dialog = (GameObject)Resources.Load("Prefabs/CommonDialog");
+            commonDialog = Instantiate(commonDialog, goDialogRoot.transform, worldPositionStays: false);
 
 
     }
@@ -61,16 +55,4 @@ public class TitleController : MonoBehaviour {
         SceneManager.LoadScene("gasha");
     }
 
-    public void XButtonDown()
-    {
-        audioSource.Play();
-        //Panel.SetActive(false);
-        //TextPanel.SetActive(false);
-        //tutorialText.SetActive(false);
-        //XButton.SetActive(false);
-        Destroy(dialogObj);
-
-
-        SaveManager.shokaititle = 1;
-    }
 }
